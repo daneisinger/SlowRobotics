479c4a8dbcd39619340a0b4f95cc214754e65471
camnewnham <camnewnham@gmail.com>
2017-02-23 12:56:36 +1100

    Added Plane3DKDTree class and KDTree dll.

--- SlowRobotics/Spatial/Plane3DOctree.cs
+++ SlowRobotics/Spatial/Plane3DOctree.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KDTree;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,6 +38,43 @@ namespace SlowRobotics.Spatial
         public void AddAll(List<Vec3D> pts)
         {
             tree.addAll(pts);
+        }
+    }
+
+    public class Plane3DKDTree : ISearchable
+    {
+        private KDTree<Vec3D> tree;
+
+        public Plane3DKDTree()
+        {
+            tree = new KDTree<Vec3D>(3);
+        }
+
+        public List<Vec3D> Search(Vec3D pt, float radius)
+        {
+            return tree.NearestNeighbors(new double[] { pt.x, pt.y, pt.z }, 1024, radius * radius).ToList();
+        }
+
+        public void Add(Vec3D pt)
+        {
+            tree.AddPoint(new double[] { pt.x, pt.y, pt.z }, pt);
+        }
+
+        public void Update(IEnumerable<Vec3D> pts)
+        {
+            tree = new KDTree<Vec3D>(3);
+            foreach (Vec3D v in pts)
+            {
+                Add(v);
+            }
+        }
+
+        public void AddAll(List<Vec3D> pts)
+        {
+            foreach (Vec3D v in pts)
+            {
+                Add(v);
+            }
         }
     }
 }
