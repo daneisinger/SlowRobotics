492693746761d6cc2a696f7b8f1b743d18280ca6
camnewnham <camnewnham@gmail.com>
2017-02-24 15:02:44 +1100

    Added SRLinearParticle

---
+++ SlowRobotics/Core/SRLinearParticle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toxiclibs.core;
+
+namespace SlowRobotics.Core
+{
+    public class SRLinearParticle : SRParticle
+    {
+        public double length;
+
+        public SRLinearParticle(float _x, float _y, float _z) : this(new Vec3D(_x, _y, _z)) { }
+        public SRLinearParticle(Vec3D _o) : base(_o) { }
+        public SRLinearParticle(Plane3D _p) : base(_p) { }
+
+
+    }
+}
