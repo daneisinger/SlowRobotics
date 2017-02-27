3e9f7f1fd305e281bf844bb0630dcf5a393bb4fc
Gwyllim Jahn <gwyllo@gmail.com>
2017-02-27 14:40:25 +1100

    Added SRString wrapper class for writing behaviours that manipulate strings

---
+++ SlowRobotics/Utils/SRString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowRobotics.Utils
+{
+    /// <summary>
+    /// Wrapper for string to be used with behaviours
+    /// </summary>
+    public class SRString
+    {
+        public String s { get; set; }
+
+        public SRString (String _s)
+        {
+            s = _s;
+        }
+        public SRString()
+        {
+            s = "";
+        }
+
+    }
+}
